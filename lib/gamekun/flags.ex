7d79ca7d889d4ebe9469b4b9ab50c17ecdeef087
J-Cotter <jamescotter@pm.me>
2019-02-13 21:59:46 +0000

    Flags Almost Done
    Flags finished apart from requirement for opcodes 0xE8 and 0xF8

---
+++ lib/gamekun/flags.ex
@@ -0,0 +1,35 @@
+defmodule GameKun.Flags do
+  use Bitwise
+
+  def z_f(<<0>>), do: 128
+  def z_f(_n), do: 0
+
+  def c_f_8(n, arg1, arg2, carry \\ 0)
+
+  def c_f_8(0, arg1, arg2, carry) do
+    (arg1 + arg2 + carry &&& 0x100) >>> 4
+  end
+
+  def c_f_8(1, arg1, arg2, carry) do
+    if arg1 - arg2 - carry < 0, do: 0, else: 0x10
+  end
+
+  def h_f_8(n, arg1, arg2, carry \\ 0)
+
+  def h_f_8(0, arg1, arg2, carry) do
+    (((arg1 &&& 0xF) + (arg2 &&& 0xF) + carry) &&& 0x10) <<< 1
+  end
+
+  def h_f_8(1, arg1, arg2, carry) do
+    if ((arg1 &&& 0xF) - (arg2 &&& 0xF) - carry) < 0, do: 0, else: 0x20
+  end
+
+  def c_f_16(arg1, arg2) do
+    (arg1 + arg2) >>> 12 &&& 0x10
+  end
+
+  def h_f_16(arg1, arg2) do
+    (arg1 &&& 0x0FFF) + (arg2 &&& 0x0FFF) >>> 7 &&& 0x20
+  end
+
+end
---
+++ test/flags_test.exs
@@ -0,0 +1,73 @@
+defmodule GameKunFlagsTest do
+  use ExUnit.Case
+  doctest GameKun.Flags
+  alias GameKun.Flags
+
+  test "Z" do
+    assert Flags.z_f(<<0>>) == 128
+    assert Flags.z_f(<<1>>) == 0
+  end
+
+  test "C-8" do
+    # Addition
+    assert Flags.c_f_8(0, 0, 0) == 0
+    assert Flags.c_f_8(0, 100, 100) == 0
+    assert Flags.c_f_8(0, 200, 200) == 16
+    assert Flags.c_f_8(0, 255, 1) == 16
+    # Addition w/ Carry
+    assert Flags.c_f_8(0, 0, 0, 1) == 0
+    assert Flags.c_f_8(0, 100, 100, 1) == 0
+    assert Flags.c_f_8(0, 200, 200, 1) == 16
+    assert Flags.c_f_8(0, 254, 1, 1) == 16
+    # Subtraction
+    assert Flags.c_f_8(1, 0, 0) == 16
+    assert Flags.c_f_8(1, 100, 100) == 16
+    assert Flags.c_f_8(1, 30, 200) == 0
+    assert Flags.c_f_8(1, 1, 25) == 0
+    # Subtraction w/ Carry
+    assert Flags.c_f_8(1, 0, 0, 1) == 0
+    assert Flags.c_f_8(1, 100, 99, 1) == 16
+    assert Flags.c_f_8(1, 200, 200, 1) == 0
+    assert Flags.c_f_8(1, 254, 1, 1) == 16
+  end
+
+  test "H-8" do
+    # Addition
+    assert Flags.h_f_8(0, 0, 0) == 0
+    assert Flags.h_f_8(0, 100, 100) == 0
+    assert Flags.h_f_8(0, 200, 200) == 32
+    assert Flags.h_f_8(0, 255, 1) == 32
+    # Addition w/ Carry
+    assert Flags.h_f_8(0, 0, 0, 1) == 0
+    assert Flags.h_f_8(0, 100, 100, 1) == 0
+    assert Flags.h_f_8(0, 199, 200, 1) == 32
+    assert Flags.h_f_8(0, 254, 1, 1) == 32
+    assert Flags.h_f_8(0, 255, 1, 1) == 32
+    # Subtraction
+    assert Flags.h_f_8(1, 0, 0) == 32
+    assert Flags.h_f_8(1, 100, 100) ==32
+    assert Flags.h_f_8(1, 30, 200) == 32
+    assert Flags.h_f_8(1, 1, 25) == 0
+    # Subtraction w/ Carry
+    assert Flags.h_f_8(1, 0, 0, 1) == 0
+    assert Flags.h_f_8(1, 100, 99, 1) == 32
+    assert Flags.h_f_8(1, 200, 200, 1) == 0
+    assert Flags.h_f_8(1, 254, 1, 1) == 32
+  end
+
+  test "C-16" do
+    assert Flags.c_f_16(0, 0) == 0
+    assert Flags.c_f_16(0xFF, 0xFF) == 0
+    assert Flags.c_f_16(0xFFFF, 1) == 0x10
+    assert Flags.c_f_16(0xFFFF, 0xFFFF) == 0x10
+  end
+
+  test "H-16" do
+    assert Flags.h_f_16(0, 0) == 0
+    assert Flags.h_f_16(0xFF, 0xFF) == 0
+    assert Flags.h_f_16(0xFFFF, 1) == 0x20
+    assert Flags.h_f_16(0xFFFF, 0xFFFF) == 0x20
+    assert Flags.h_f_16(0x0FFF, 0) == 0
+    assert Flags.h_f_16(0x0FFF, 1) == 0x20
+  end
+end
